c0f6e161a445be48145f8a8c8aeb395bc52a11a2
Ryan Waggoner <ryan@waggoner.co>
2021-02-10 05:03:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 && int % 5 == 0
-    "FizzBuzz"
+  if int % 3 == 0
+    "Fizz"
   else int % 5 == 0
     "Buzz"
-  else int % 3 == 0
-    "Fizz"
+  else int % 3 && int % 5 == 0
+    "FizzBuzz"
   else int % 4 == 0
     nil
   end
