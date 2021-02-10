bccbe736e48e49cc3a308d5cc0c15985b439590c
Ryan Waggoner <ryan@waggoner.co>
2021-02-10 05:16:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,19 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
+end
+
+def fizzbuzz(int)
   if int % 5 == 0
     "Buzz"
+end
+
+def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+end
+
+def fizzbuzz(int)
   else int % 4 == 0
     nil
-  end
 end
